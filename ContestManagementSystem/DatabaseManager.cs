f58f3c415e39afb7a684c75d686850f5d7d27aca
rlsadiz <ronald.louie.sadiz@gmail.com>
2015-07-04 06:11:15 +0800

    Database Classes
    Added ContestDatabase.mdf
    Added DatabaseManager.cs

---
+++ ContestManagementSystem/DatabaseManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContestManagementSystem
+{
+    public class DatabaseManager
+    {
+        public DataTable LoadTable()
+        {
+           throw new NotImplementedException;
+        }
+
+        public bool SaveTable(DataTable table, String tableName)
+        {
+            throw new NotImplementedException;
+        }
+    }
+}
