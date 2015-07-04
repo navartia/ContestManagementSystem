41251a8de39fbbdff00f80ee8791075d268490e5
rlsadiz <ronald.louie.sadiz@gmail.com>
2015-07-04 22:34:24 +0800

    Database Classes
    Added ContestantScoreView

--- ContestManagementSystem/DatabaseManager.cs
+++ ContestManagementSystem/DatabaseManager.cs
@@ -7,8 +7,20 @@ using System.Threading.Tasks;
 
 namespace ContestManagementSystem
 {
+    using ContestantTable = ContestDataSet.ContestantDataTable;
+    using ScoreTable = ContestDataSet.ScoreDataTable;
+
     public class DatabaseManager
     {
+        private ContestantTable tableContestant;
+        private ScoreTable tableScore;
+
+        public DatabaseManager()
+        {
+            tableContestant = new ContestantTable();
+            tableScore = new ScoreTable();
+        }
+
         public DataTable LoadTable()
         {
            throw new NotImplementedException();
