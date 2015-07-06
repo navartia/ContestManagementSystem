0dd32bb671454d2f7c65dbbba2e797e52e172f75
rlsadiz <ronald.louie.sadiz@gmail.com>
2015-07-06 23:10:51 +0800

    Merging stability
    Cleanup for merging stability

---
+++ ContestManagementSystem/LoginForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContestManagementSystem
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRegistration_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonJudging_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
