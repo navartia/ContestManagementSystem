fa1b5e48a78612c4050a87e50b92c523139f3856
kmdriz <kimimayzird00@gmail.com>
2015-08-02 11:51:13 +0800

    DGV

--- ContestManagementSystem/SingingIdol.cs
+++ ContestManagementSystem/SingingIdol.cs
@@ -23,69 +23,9 @@ namespace ContestManagementSystem
             dm = new DatabaseManager();
             contestantList = new ArrayList();
         }
-
-        private void hScrollBarVQ_Scroll(object sender, ScrollEventArgs e)
-        {
-            int value = hScrollBarVQ.Value;
-            float adjustedValue = value / 91f * 50f;
-
-            textBoxVQ.Text = Convert.ToInt32(adjustedValue).ToString();
-        }
-
-        private void hScrollBarOrg_Scroll(object sender, ScrollEventArgs e)
-        {
-            int value = hScrollBarOrg.Value;
-            float adjustedValue = value / 91f * 30f;
-
-            textBoxOrg.Text = Convert.ToInt32(adjustedValue).ToString();
-        }
-
-        private void hScrollBarSQ_Scroll(object sender, ScrollEventArgs e)
-        {
-            int value = hScrollBarSQ.Value;
-            float adjustedValue = value / 91f * 10f;
-
-            textBoxSQ.Text = Convert.ToInt32(adjustedValue).ToString();
-        }
-
-        private void hScrollBarSP_Scroll(object sender, ScrollEventArgs e)
-        {
-            int value = hScrollBarSP.Value;
-            float adjustedValue = value / 91f * 10f;
-
-            textBoxSP.Text = Convert.ToInt32(adjustedValue).ToString();
-        }
-
-        private void buttonClear_Click(object sender, EventArgs e)
-        {
-            hScrollBarVQ.Value = 0;
-            hScrollBarOrg.Value = 0;
-            hScrollBarSQ.Value = 0;
-            hScrollBarSP.Value = 0;
-
-            textBoxVQ.Text = "0";
-            textBoxOrg.Text = "0";
-            textBoxSQ.Text = "0";
-            textBoxSP.Text = "0";
-
-            int[] score = new int[4];
-            score[0] = Convert.ToInt32(textBoxVQ.Text);
-            score[1] = Convert.ToInt32(textBoxOrg.Text);
-            score[2] = Convert.ToInt32(textBoxSQ.Text);
-            score[3] = Convert.ToInt32(textBoxSP.Text);
-
-            int index = comboBoxContestant.SelectedIndex;
-            Contestant selected = contestantList[index] as Contestant;
-            selected.score = score;
-        }
-
         private void buttonSubmit_Click(object sender, EventArgs e)
         {
             int[] score = new int[4];
-            score[0] = Convert.ToInt32(textBoxVQ.Text);
-            score[1] = Convert.ToInt32(textBoxOrg.Text);
-            score[2] = Convert.ToInt32(textBoxSQ.Text);
-            score[3] = Convert.ToInt32(textBoxSP.Text);
 
             int index = comboBoxContestant.SelectedIndex;
             Contestant selected = contestantList[index] as Contestant;
@@ -203,15 +143,6 @@ namespace ContestManagementSystem
             labelNumber.Text = Convert.ToString(selected.contestant_number);
 
             int[] score = selected.score;
-            textBoxVQ.Text = Convert.ToString(score[0]);
-            textBoxOrg.Text = Convert.ToString(score[1]);
-            textBoxSQ.Text = Convert.ToString(score[2]);
-            textBoxSP.Text = Convert.ToString(score[3]);
-
-            hScrollBarVQ.Value = Convert.ToInt32(score[0] / 50f * 91f);
-            hScrollBarOrg.Value = Convert.ToInt32(score[1] / 30f * 91f);
-            hScrollBarSQ.Value = Convert.ToInt32(score[2] / 10f * 91f);
-            hScrollBarSP.Value = Convert.ToInt32(score[3] / 10f * 91f);
         }
     }
 }
